a3075726ad72f11925edbe29fe0d7eeb38db9b65
gongqungeng <gongqungeng@163.com>
2018-11-15 22:06:02 +0800

    测试练习

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -1,9 +1,13 @@
 import org.junit.Test;
 
+/**
+ * git测试
+ */
 public class Main {
 
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
+        System.out.println("龚群耕 666");
     }
 }
